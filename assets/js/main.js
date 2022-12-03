41386ce62ae911b0b15948eb479c102cf8681689
Linh <lixxkook@Macbut-M1-cua-Linh.local>
2022-12-03 19:38:43 +0700

    uipdayte

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+﻿const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const toggleNavMobile = () => {
+    $('.soft-menu').classList.toggle('open')
+    $('.soft-menu__list').classList.toggle('open')
+}
+
+$('.soft-menu__list').onclick = (e) => {
+    e.stopPropagation()
+}
